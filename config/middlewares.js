a2ce1e4845bf3d649291eea8a00d37199ec64308
kirankattii <kirankatti550@gmail.com>
2025-09-28 02:39:32 +0530

    updated vercel and cloudinary config

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,47 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://market-assets.strapi.io',
+            'https://console.cloudinary.com',
+            'https://res.cloudinary.com',
+          ],
+          'script-src': [
+            "'self'",
+            'example.com',
+            'https://media-library.cloudinary.com',
+            'https://upload-widget.cloudinary.com',
+            'https://console.cloudinary.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://console.cloudinary.com',
+            'https://res.cloudinary.com',
+          ],
+          'frame-src': [
+            "'self'",
+            'https://media-library.cloudinary.com',
+            'https://upload-widget.cloudinary.com',
+            'https://console.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+    'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,10 @@
-module.exports = () => ({});
+export default {
+    'cloudinary-media-library': {
+      enabled: true,
+      config: {
+        cloudName: 'di3n2jdpt',
+        apiKey: '983685577811437',
+        encryptionKey: 'mzTNLQjb_Nn7nF527zs1QopuwsM',
+      },
+    },
+  };
